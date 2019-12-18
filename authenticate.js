bacb64bb32919cafe3d3704ad77677dd4edc8bd9
sagarpednekar <sagarpednekar04@gmail.com>
2019-12-18 18:58:37 +0530

    Feat(API's for token based auth)

---
+++ authenticate.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+module.exports = (req, res, next) => {
+   const token = req.headers.authorization;
+
+   if (token) {
+      jwt.verify(token, process.env.tokenSecret, (err, decoded) => {
+         if (err) {
+            // unauthorized
+
+            return res
+               .status(401)
+               .send({ error: true, msg: "Unauthorized user" });
+         }
+         console.log(decoded);
+         req.decoded = decoded;
+         next();
+      });
+   } else {
+      res.status(403).send({ error: true, msg: "Token not provided" });
+   }
+};
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const app = express();
+const authorize = require("./authenticate");
+const tokenList = {};
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+// add middleware
+
+app.use(bodyParser.json());
+
+// root path
+
+app.get("/", (req, res) => {
+   res.send("APP is up and running");
+});
+
+app.post("/login", (req, res) => {
+   const requestBody = req.body;
+   /**
+    * Database authentication goes here
+    */
+   const user = {
+      email: requestBody.email,
+      password: requestBody.password
+   };
+
+   // generate token
+
+   /**
+    *
+    * {payload } - payload object
+    * { secret } - secret key to generate token
+    * { expiresIn} - expiration time
+    *
+    */
+
+   const token = jwt.sign(user, process.env.tokenSecret, {
+      expiresIn: "10m"
+   });
+   const refreshToken = jwt.sign(user, process.env.refreshTokenSecret, {
+      expiresIn: process.env.refreshTokenLife
+   });
+
+   tokenList[refreshToken] = { token, refreshToken };
+   res.status(200).json({
+      status: "Logged In",
+      token,
+      refreshToken
+   });
+});
+
+app.post("/token", (req, res) => {
+   const reqBody = req.body;
+   if (reqBody.refreshToken && reqBody.refreshToken in tokenList) {
+      const user = {
+         email: reqBody.email,
+         password: reqBody.password
+      };
+
+      const token = jwt.sign(user, process.env.tokenSecret, {
+         expiresIn: "10m"
+      });
+
+      console.log(tokenList);
+      // update the token in list
+      tokenList[reqBody.refreshToken].token = token;
+      console.log(tokenList);
+      res.status(200).send({
+         token
+      });
+   } else {
+      // missing refreshToken
+      res.status(404).send("Unauthorized user");
+   }
+});
+
+app.get("/secure", authorize, (req, res) => {
+   res.send({
+      Msg: "Now I can access the secure API"
+   });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
